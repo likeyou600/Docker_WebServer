34d5926df6e7d9681b56ea2f56dcdf963980c872
BAKERYCHU <58775534+likeyou600@users.noreply.github.com>
2024-01-27 20:17:55 +0800

    Update backup_db.sh

--- backup_db.sh
+++ backup_db.sh
@@ -1,8 +1,8 @@
 CONTAINER=XXXXX
 DB_NAME=XXXXX
-FILENAME=/db_backup/${DB_NAME}_$(date "+%Y%m%d_%H%M%S").dump
+FILENAME=/db_backup/${DB_NAME}_$(date "+%Y%m%d_%H%M%S").sql
 if [ ! -d "/db_backup" ]; then
     mkdir -p "/db_backup"
 fi
 
-docker exec ${CONTAINER} sh -c "mysqldump -u root ${DB_NAME}" > ${FILENAME}
+docker exec ${CONTAINER} sh -c "mysqldump --user=XXXXX --password=XXXXX --skip-add-locks ${DB_NAME}" > ${FILENAME}
