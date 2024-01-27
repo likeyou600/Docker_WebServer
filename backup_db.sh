1ec5a817ef8ba26af770f44d7bb69d839a5dc794
BAKERYCHU <58775534+likeyou600@users.noreply.github.com>
2024-01-27 20:07:14 +0800

    Update backup_db.sh

--- backup_db.sh
+++ backup_db.sh
@@ -1,5 +1,8 @@
 CONTAINER=XXXXX
 DB_NAME=XXXXX
 FILENAME=/db_backup/${DB_NAME}_$(date "+%Y%m%d_%H%M%S").dump
+if [ ! -d "/db_backup" ]; then
+    mkdir -p "/db_backup"
+fi
 
 docker exec ${CONTAINER} sh -c "mysqldump -u root ${DB_NAME}" > ${FILENAME}
